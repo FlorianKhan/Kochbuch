f5dbe0b055b4cd367947796fddd3b75013715915
angelikakleimann <angelika.kleimann01@gmail.com>
2022-05-08 19:46:49 +0200

    Anpassung der Kommentare

--- Mein digitales Kochbuch/Frontend/src/backend.js
+++ Mein digitales Kochbuch/Frontend/src/backend.js
@@ -38,9 +38,9 @@ export default class Backend {
   * Low-level Wrapper um die Fetch API, um diese bei jeder Verwendung mit
   * denselben Parametern zu versorgen. Aufrufbeispiel:
   *
-  *   * backend.fetch("GET", "/address");
-  *   * backend.fetch("GET", "/address", {query: {first_name: "Test"}});
-  *   * backend.fetch("PUT", "/address", {body: { ... }});
+  *   * backend.fetch("GET", "/rezept");
+  *   * backend.fetch("GET", "/rezept", {query: {rezeptname: "Test"}});
+  *   * backend.fetch("PUT", "/rezept", {body: { ... }});
   *
   * Über das Options-Objekt können Query-Parameter mitgegeben werden, die
   * als URL-Parameter angehängt werden, sowie Daten, die nach JSON konvertiert
